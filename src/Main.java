c86ce074f9bbd4a743edd18fca6517927a25fc52
Ricardo Macedo <ricardomacedor@icloud.com>
2025-02-21 16:47:23 -0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,55 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        int number1, number2;
+        String texto = """
+                welcome to the program!
+                choose one of the options
+                1- addition
+                2- subtraction
+                3- multiplication
+                4- division
+                5- square root
+                6- percentage
+                """;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println(texto);
+        System.out.print("Enter your choice: ");
+        int choice = scanner.nextInt();
+
+            System.out.println("enter a number: ");
+            number1 = scanner.nextInt();
+            System.out.println("enter another number: ");
+            number2 = scanner.nextInt();
+
+            switch (choice){
+                case 1:
+                    System.out.println("result: " + (number1 + number2));
+                    break;
+                case 2:
+                    System.out.println("result: " + (number1 - number2));
+                    break;
+                case 3:
+                    System.out.println("result: " + (number1 * number2));
+                    break;
+                case 4:
+                    System.out.println("result: " + (number1 / number2));
+                    break;
+                case 5:
+                    if (number1 >= 0){
+                        System.out.println("Square root: " + Math.sqrt(number1));
+                    } else {
+                        System.out.println("Error: Square root is negative");
+                    }
+                    break;
+                case 6:
+                    System.out.println("porcentagem: " + ((number1 / 100.0) * number2) + "%");
+                    break;
+                default:
+                    System.out.println("Error: Invalid choice");
+            }
+            scanner.close();
         }
     }
-}
